860f2f0458aa2d33a3cc98464c936bdd1818e8ea
Maks0123 <vadimcorporateukr@gmail.com>
2022-04-18 23:25:51 +0300

    func

--- main.go
+++ main.go
@@ -2,28 +2,75 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"runtime"
 )
 
 func main() {
-	runtime.GOMAXPROCS(1)
-	HandleFunction()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/time", time)
+	mux.HandleFunc("/health", health)
+
+	err := http.ListenAndServe(":3000", mux)
+	log.Fatal(err)
 }
 
-func HandleFunction() {
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 
-	var port = "8080"
-	r := mux.NewRouter()
+	// Здесь может быть общий код для всех запросов...
 
-	r.HandleFunc("/", TestFunc).Methods("GET")
+	switch r.Method {
+	case http.MethodGet:
+		// Обработка GET запроса...
 
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	case http.MethodPost:
+		// Обработка POST запроса...
+
+	case http.MethodOptions:
+		w.Header().Set("Allow", "GET, POST, OPTIONS")
+		w.WriteHeader(http.StatusNoContent)
+
+	default:
+		w.Header().Set("Allow", "GET, POST, OPTIONS")
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
-func TestFunc(w http.ResponseWriter, r *http.Request) {
-	var hello string = "Hello GO"
+func time(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/time" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+
+	var test = "TEST!"
+	html := `<doctype html>
+        <html>
+        <head>
+          <title>Hello World</title>
+        </head>
+        <body>
+        <p>
+          <a href='/welcome'>Welcome `{{ test }}` </a> | <a href='/message'>Message</a>
+        </p>
+        </body>
+       </html>`
+	fmt.Fprintln(w, html)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/health" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(hello)
+	json.NewEncoder(w).Encode(http.StatusOK)
 }
